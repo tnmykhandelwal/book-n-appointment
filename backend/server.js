4f17c6912b801df6213219d7e54bdffd6b516647
Tanmay Kumar Khandelwal <tnmykhandelwal1@gmail.com>
2026-06-29 10:03:56 +0530

    Initial Commit : set up of the project structure

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Book N\' Appointment Server API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
